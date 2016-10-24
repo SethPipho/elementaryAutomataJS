32a690d59ffde1c1270dfa0fdc282aaedba9ce58
SethPipho <seth.pipho@gmail.com>
2016-10-23 21:39:18 -0500

    init commit

---
+++ js/demo.js
@@ -0,0 +1,8 @@
+window.onload = function()
+{
+    canvas = document.getElementById("canvas")
+    ctx = canvas.getContext("2d")
+
+    canvas.height = window.innerHeight
+    canvas.width = window.innerWidth
+}
